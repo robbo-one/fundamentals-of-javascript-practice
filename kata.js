4e376f54d9505ba67321cdadf23778589ef0d5a9
student <student@eda-bc-07.localdomain>
2021-01-04 16:50:02 +1300

    Answered Kata Objects and Arrays as best as we could.

--- kata.js
+++ kata.js
@@ -5,11 +5,16 @@
 // getGreeting should return a string containing
 // 'Hello ' and the contents of `name`
 function getGreeting (name) {
+  return "Hello " + name
 }
 
 // ageOneYear should return a new object with an `age` property 1 greater
 // than the `age` property of `obj`
 function ageOneYear (obj) {
+  const newObject = {
+    age: obj.age + 1
+  }
+  return newObject
 }
 
 // makeObject should return an object that looks like this:
@@ -18,23 +23,40 @@ function ageOneYear (obj) {
 //   key: value
 // }
 function makeObject (key, value) {
+  const newObject = {
+    
+  }
+  newObject[key] = value
+  //console.log(newObject)
+  return newObject
 }
 
 // getPropertyValue should return the value of the
 // property contained in the `key` of `obj`
 function getPropertyValue (obj, key) {
+  return obj[key]
 }
 
 // addName should return a copy of `obj` with the addition of a `name`
 // property that has the value of the `name` argument
 // Tip: consider the object literal spread syntax
 function addName (obj, name) {
+  const newObject = {
+    ...obj
+  }
+  newObject.name = name
+  return newObject
 }
 
 // deleteProperty should return a new copy of `obj` without the property name
 // that matches the `key` parameter
 // Tip: consider JavaScript's `delete` operator
 function deleteProperty (obj, key) {
+  const newObject = {
+    ...obj
+  }
+  delete newObject[key]
+  return newObject
 }
 
 // returnErrorIfFalsy should return a JavaScript Error object with message:
@@ -42,16 +64,27 @@ function deleteProperty (obj, key) {
 // if val evaluates to false
 // Tip: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
 function returnErrorIfFalsy (val) {
+  if (val == false){
+    return new Error("Oh no, an error!")
+  }
+
+  //console.log(val)
 }
 
 // keys should return an array of the object's property names (keys)
 // For example, given { foo: 1, bar: 2 } it would return ['foo', 'bar']
 function getKeys (obj) {
+  return Object.keys(obj)
 }
 
 // getValues should return an array of the object's own values
 // For example, given { foo: 1, bar: 2 } it would return [1, 2]
 function getValues (obj) {
+  const arr = []
+  for(var key in obj){
+    arr.push(obj[key])
+  }
+  return arr
 }
 
 /**
@@ -62,28 +95,46 @@ function getValues (obj) {
 // `item`. For example, makeArrayOfItem('foo', 2) would return:
 // ['foo', 'foo']
 function makeArrayOfItem (item, length) {
+  //console.log(item, length)
+  const arr = []
+  for(var i = 0; i < length; i++){
+    arr.push(item)
+  }
+  return arr
 }
 
 // makeArrayOfItems should return an array containing all arguments passed to it
 // Tip: consider JavaScript's Rest parameters
-function makeArrayOfItems () {
+function makeArrayOfItems (...theArgs) {
+  return theArgs
 }
 
 // hasItem should return true if `item` is present in `arr` at least once,
 // otherwise it should return false.
 // Tip: there is an array function that makes this straightforward
 function hasItem (arr, item) {
+  return arr.includes(item)
 }
 
 // getItemAtIndex should return arr[idx] but only if that index exists:
 // if it doesn't, return a JavaScript Error object.
 function getItemAtIndex (arr, idx) {
+  //console.log(arr, idx)
+  if(idx >= arr.length){
+    return new Error()
+  } else {
+    return arr[idx]
+  }
 }
 
 // replaceItemAtIndex should return a copy of `arr` with
 // the element at `idx` replaced with `item`
 // Tip: consider the array literal spread syntax
 function replaceItemAtIndex (arr, idx, item) {
+  const newArray = [
+    ...arr
+  ]
+  console.log(newArray)
 }
 
 // insertItemAtIndex should return a copy of `arr` with `item` inserted at
