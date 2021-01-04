97232e9b15bc76ae1c8da605eafad74ee93f4a18
student <student@eda-bc-04.localdomain>
2021-01-04 16:51:13 +1300

    finished first four katas

--- kata.js
+++ kata.js
@@ -5,11 +5,17 @@
 // getGreeting should return a string containing
 // 'Hello ' and the contents of `name`
 function getGreeting (name) {
+  return "Hello " + name
 }
 
 // ageOneYear should return a new object with an `age` property 1 greater
 // than the `age` property of `obj`
 function ageOneYear (obj) {
+  newObject = {
+    age : obj.age + 1
+  }
+
+ return newObject
 }
 
 // makeObject should return an object that looks like this:
@@ -18,17 +24,30 @@ function ageOneYear (obj) {
 //   key: value
 // }
 function makeObject (key, value) {
+  newObject = {
+   //[key] : value
+  }
+  newObject[key] = value
+ console.log(newObject)
+ return newObject
 }
+
 
 // getPropertyValue should return the value of the
 // property contained in the `key` of `obj`
 function getPropertyValue (obj, key) {
+  return obj[key]
 }
+
+
 
 // addName should return a copy of `obj` with the addition of a `name`
 // property that has the value of the `name` argument
 // Tip: consider the object literal spread syntax
 function addName (obj, name) {
+  obj[name] = name
+  return obj 
+console.log ()
 }
 
 // deleteProperty should return a new copy of `obj` without the property name
