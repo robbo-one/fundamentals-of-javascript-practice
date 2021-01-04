b7f62939905d122baf57f73905a241f840875f99
Mike Parkin <michael.parkin1988@gmail.com>
2021-01-04 18:49:43 +1300

    test getItemAtIndex passed

--- kata.js
+++ kata.js
@@ -126,6 +126,11 @@ function hasItem (arr, item) {
 // getItemAtIndex should return arr[idx] but only if that index exists:
 // if it doesn't, return a JavaScript Error object.
 function getItemAtIndex (arr, idx) {
+  if (arr[idx]) {
+    return arr[idx] 
+  } else {
+    return Error() 
+  }
 }
 
 // replaceItemAtIndex should return a copy of `arr` with
