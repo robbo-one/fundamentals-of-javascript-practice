e25ed14be37621b8bf9ad29f858200ac90d894eb
Tom May <tommay90@gmail.com>
2021-01-05 22:04:44 +1300

    Complete more Katas.

--- kata.js
+++ kata.js
@@ -5,11 +5,17 @@
 // getGreeting should return a string containing
 // 'Hello ' and the contents of `name`
 function getGreeting (name) {
+  return "Hello " + name
 }
 
 // ageOneYear should return a new object with an `age` property 1 greater
 // than the `age` property of `obj`
 function ageOneYear (obj) {
+  newObj = {
+ age : ++obj.age
+  }
+ return newObj
+ 
 }
 
 // makeObject should return an object that looks like this:
@@ -18,23 +24,33 @@ function ageOneYear (obj) {
 //   key: value
 // }
 function makeObject (key, value) {
+  newObject = {}
+  newObject[key] = value
+ return newObject
 }
 
 // getPropertyValue should return the value of the
 // property contained in the `key` of `obj`
 function getPropertyValue (obj, key) {
+  return obj[key]
 }
 
 // addName should return a copy of `obj` with the addition of a `name`
 // property that has the value of the `name` argument
 // Tip: consider the object literal spread syntax
 function addName (obj, name) {
+  var newObj = {...obj}
+  newObj.name = name
+  return newObj
 }
 
 // deleteProperty should return a new copy of `obj` without the property name
 // that matches the `key` parameter
 // Tip: consider JavaScript's `delete` operator
 function deleteProperty (obj, key) {
+  var newObj = {...obj}
+  delete newObj[key]
+  return newObj
 }
 
 // returnErrorIfFalsy should return a JavaScript Error object with message:
@@ -42,16 +58,21 @@ function deleteProperty (obj, key) {
 // if val evaluates to false
 // Tip: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
 function returnErrorIfFalsy (val) {
+  if (val == false) {
+    return Error('Oh no, an error!')
+  }
 }
 
 // keys should return an array of the object's property names (keys)
 // For example, given { foo: 1, bar: 2 } it would return ['foo', 'bar']
 function getKeys (obj) {
+  return Object.keys(obj)
 }
 
 // getValues should return an array of the object's own values
 // For example, given { foo: 1, bar: 2 } it would return [1, 2]
 function getValues (obj) {
+  return Object.values(obj)
 }
 
 /**
@@ -62,11 +83,15 @@ function getValues (obj) {
 // `item`. For example, makeArrayOfItem('foo', 2) would return:
 // ['foo', 'foo']
 function makeArrayOfItem (item, length) {
+  return new Array(length).fill(item)
+  
 }
 
 // makeArrayOfItems should return an array containing all arguments passed to it
 // Tip: consider JavaScript's Rest parameters
-function makeArrayOfItems () {
+function makeArrayOfItems (...args) {
+  var arr = args
+  return arr
 }
 
 // hasItem should return true if `item` is present in `arr` at least once,
